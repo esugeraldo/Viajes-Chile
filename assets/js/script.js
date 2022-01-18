f75bcf9e76ae11c233947072cd300924c232a15d
Esteban Geraldo <esugeraldo@gmail.com>
2022-01-18 20:48:11 -0300

    navegador con smooth scroll js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Smooth scroll en navegador
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 500, function(){
+     
+
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
